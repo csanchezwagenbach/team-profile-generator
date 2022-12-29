dc656c45f56fa7bedac4f79fa3bfd44deb09dd62
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-12-29 11:55:43 -0800

    created question objects for creation of manager object

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const template = require("./src/template");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your team's manager?"
+        },
+        {
+           type: "input",
+           name: "id",
+           message: "What is the ID number of your team's manager?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the email address of your team's manager?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the office number of your team's manager?"
+        }
+    ])
